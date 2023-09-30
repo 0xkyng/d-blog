420cb4617e12ce780c693b099d7965c713be04a4
Isaac <teryima127@gmail.com>
2023-09-30 12:24:18 +0100

    buttons

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,32 @@
+import Button from "../components/Button";
+import CardList from "../components/CardLIsit";
+
+const handleClick = () => {
+    alert('Button clicked!');
+  };
+
+
+  const cards = [
+    {
+      title: 'Card 1',
+      content: 'Content for Card 1',
+    },
+    {
+      title: 'Card 2',
+      content: 'Content for Card 2',
+    },
+    {
+      title: 'Card 3',
+      content: 'Content for Card 3',
+    }
+  ];
+
 const Home = () => {
     return (
         <>
-        This is the Homepage
+        
+        <Button text="Click me" onClick={handleClick} className="mt-4" />
+        <CardList cards={cards}/>
         </>
     )
 }
